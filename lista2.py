1d2947669ab10a9cf7c5cfe50c3e3c2df3dd97ae
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2024-04-04 10:36:17 -0300

    Update lista2.py

--- lista2.py
+++ lista2.py
@@ -1,8 +1,11 @@
+import os
+
 def mostrarLista(lista):
     print("Lista atual:", lista)
 
 def gravarLista(vLista):
     nomeArq = input("Qual o nome que você deseja para o arquivo?")
+    nomeArq += ".txt"  # Adiciona .txt ao final do nome do arquivo
     with open(nomeArq, "w") as arquivo:
         for item in vLista:
             arquivo.write(item + "\n")
@@ -10,6 +13,7 @@ def gravarLista(vLista):
 
 def carregarLista():
     nomeArq = input("Digite o nome do arquivo que deseja carregar: ")
+    nomeArq += ".txt"  # Adiciona .txt ao final do nome do arquivo
     lista_carregada = []
     try:
         with open(nomeArq, "r") as arquivo:
@@ -22,13 +26,30 @@ def carregarLista():
         print("Arquivo não encontrado.")
         return []
     except Exception as e:
-        print("OCORREU UM ERRO: ",e)
+        print("OCORREU UM ERRO: ", e)
         return
     
 def ordenar_lista(lista):
-    lista_ordenada = sorted(reverse=True)
+    lista_ordenada = sorted(lista, reverse=True)
     print("Lista ordenada com sucesso:", lista_ordenada)
     return lista_ordenada
+
+def listar_arquivos():
+    # Diretório onde estão os arquivos
+    diretorio = os.getcwd()  # Obtém o diretório atual
+
+    # Lista para armazenar os nomes dos arquivos .txt
+    arquivos_txt = []
+
+    # Percorre todos os arquivos e diretórios dentro do diretório especificado
+    for nome_arquivo in os.listdir(diretorio):
+        # Verifica se o nome do arquivo termina com .txt
+        if nome_arquivo.endswith(".txt"):
+            # Se sim, adiciona à lista de arquivos .txt
+            arquivos_txt.append(nome_arquivo)
+    
+    # Retorna a lista de arquivos .txt
+    print(arquivos_txt)
 
 def main():
     lista = []
@@ -42,7 +63,8 @@ def main():
         print("4. Gravar lista")
         print("5. Carregar lista")
         print("6. Ordenar Lista")
-        print("7. Sair")
+        print("7. Diretorio.")
+        print("8. Sair")
         print("==============================================\n")
         print("----===+===----")
         escolha = input("Escolha uma opção: ")
@@ -83,7 +105,9 @@ def main():
             lista = carregarLista()
         elif escolha == "6":
             lista = ordenar_lista(lista)
-        elif escolha == '7':
+        elif escolha == "7":
+            listar_arquivos()
+        elif escolha == '8':
             print("----===+===----")
             print("Encerrando o programa.")
             print("")
